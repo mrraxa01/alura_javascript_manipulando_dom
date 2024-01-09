705e45053d2f82dd668625f77e7f5d91567dca24
Marcio Rodrigues <mrraxa01@gmail.com>
2024-01-09 19:53:03 -0300

    Project completed

--- script.js
+++ script.js
@@ -1,18 +1,112 @@
 const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
-const longoBt = document.querySelector('.app__card-button--longo')
+const longoBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const imgStartPauseBt  = document.querySelector('#start-pause img');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('/sons/beep.mp3');
+const tempoNaTela = document.querySelector('#timer');
+
+
+let intervaloId = null;
+let tempoDecorridoSegundos = 1500;
+
+musica.loop = true;
+
+musicaFocoInput.addEventListener('change', ()=>{
+    if(musica.paused){
+        musica.play();
+    }else{
+        musica.pause();
+    }
+}) 
 
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
+    tempoDecorridoSegundos = 1500;
+    alterarContexto('foco');
+    focoBt.classList.add('active');
 })
 
 curtoBt.addEventListener('click', ()=> {
-    html.setAttribute('data-contexto', 'descanso-curto');
+    tempoDecorridoSegundos = 300;
+    alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
 })
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
+    tempoDecorridoSegundos = 900;
+    alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
 })
 
+function alterarContexto(contexto){
+    mostrarTempo();
+    botoes.forEach(function(contexto){
+        contexto.classList.remove('active');
+    } );
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `imagens/${contexto}.png`);
+    switch(contexto){
+        case "foco":
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`
+        break;
 
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada?<br>
+            <strong class="app__title-strong">Faça uma pausa curta.</strong>`
+        break;
+
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar a superfície.<br>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+    }
+}
+const contagemRegressiva = () => {
+    if(tempoDecorridoSegundos <= 0){
+        audioTempoFinalizado.play();
+        alert('Tempo Finalizado!');
+        zerar();
+        return;
+    }
+    mostrarTempo();
+    tempoDecorridoSegundos -= 1;
+    console.log('Temporizador: ' + tempoDecorridoSegundos);
+}
+startPauseBt.addEventListener('click' ,inicarOuPausar);
+
+function inicarOuPausar() {
+    if(intervaloId){
+        
+        audioPausa.play();
+        zerar();
+        return;
+    }
+     audioPlay.play();
+    intervaloId = setInterval(contagemRegressiva, 1000);
+    imgStartPauseBt.setAttribute('src', "imagens/pause.png");
+    iniciarOuPausarBt.textContent = "Pausar";
+}
+
+function zerar(){
+    clearInterval(intervaloId);
+    imgStartPauseBt.setAttribute('src', "imagens/play_arrow.png");
+    iniciarOuPausarBt.textContent = "Começar";
+    intervaloId = null;
+}
+
+function mostrarTempo(){
+    const tempo = new Date(tempoDecorridoSegundos * 1000);
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'});
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo();
