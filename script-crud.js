5e11e9cac7cee6805b56d75955c651886dbbaee9
Marcio Rodrigues <mrraxa01@gmail.com>
2024-01-14 21:45:11 -0300

    Aula 4 - Integrando como cronometro

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,9 @@ const textArea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
+let tarefaSelecionada = null;
+let liTarefaSelecionada = null;
 
 function atualizarTarefas(){
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
@@ -45,6 +48,26 @@ function criarElementoTarefa(tarefa){
     li.append(paragrafo);
     li.append(botao);
 
+    li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active')
+        .forEach(elemento =>{
+            elemento.classList.remove('app__section-task-list-item-active')
+        })
+
+        if(tarefaSelecionada == tarefa){
+            paragrafoDescricaoTarefa.textContent='';
+            tarefaSelecionada = null;
+            liTarefaSelecionada = null;    
+            return;
+        }
+
+        tarefaSelecionada = tarefa;
+        liTarefaSelecionada = li;
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+      
+        li.classList.add('app__section-task-list-item-active');
+    }
+
     return li;
 }
 
@@ -77,3 +100,11 @@ tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa);
 });
+
+document.addEventListener('FocoFinalizado', ()=>{
+    if(tarefaSelecionada && liTarefaSelecionada){
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active');
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete');
+        liTarefaSelecionada.querySelector('button').setAttribute('disable', 'disable');
+    }
+})
